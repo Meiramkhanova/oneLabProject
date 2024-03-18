17199a9ac5cbe8f324dc09dac87ed3f3490db918
Adina <adina.meiramkhanova@narxoz.kz>
2024-03-18 23:27:51 +0500

    added a sign up page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
+import SignUp from "./pages/SignUp";
 
 function App() {
-  return <div>One Lab</div>;
+  return (
+    <div>
+      <Header />
+      <SignUp />
+    </div>
+  );
 }
 
 export default App;
